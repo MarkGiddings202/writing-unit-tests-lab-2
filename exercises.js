400f600480d6e8a0ff691c8e2a9fad42461121ef
gavingiddings1997@gmail.com <Gavin>
2023-01-21 14:29:34 -0500

    uploaded hw

--- exercises.js
+++ exercises.js
@@ -1,17 +1,42 @@
-function octalToDecimal() {
+function octalToDecimal(octal) {
   //Write code here
+  return parseInt(octal, 8); 
 }
 
-function anagram() {
+function anagram(word, words) {
   //Write code here
+  let newArr = []
+    for (let i = 0; i < words.length; i++) {
+      if (sortString(words[i]) === sortString(word)) {
+        newArr.push(words[i])
+      }
+    }
+    return newArr;
 }
 
-function triangle() {
-  //Write code here
+function sortString(str) {
+    return str.split('').sort().join('');
 }
 
-function fridayThe13ths() {
+function triangle(angle1, angle2, angle3) {
   //Write code here
+  if(angle1 + angle2 + angle3 !== 180 || Math.min(angle1,angle2,angle3)=== 0) return 'invalid';
+  if(Math.max(angle1, angle2, angle3) > 90) return 'obtuse';
+  if(Math.max(angle1, angle2, angle3) < 90) return 'acute';
+  return 'right';
+}
+
+function fridayThe13ths(year) {
+  //Write code here
+  let count = 0;
+  for (let month = 0; month < 12; month++) {
+    let d = new Date(year, month, 13);
+    if (d.getDay() === 5) {
+      count++;
+    }
+  }
+  return count;
+
 }
 
 module.exports = {
